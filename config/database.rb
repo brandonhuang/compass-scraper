32d4559d659cea836dbe1d11dbad5f3ff73664f0
Samson Wong <samsonw_@hotmail.com>
2015-06-27 00:04:21 +0000

    db URL

--- config/database.rb
+++ config/database.rb
@@ -4,15 +4,15 @@ configure do
     ActiveRecord::Base.logger = Logger.new(STDOUT)
   end
 
-  set :database, {
-    adapter: "postgresql",
-    database: "d1sn01s5ri49fu",
-    host: "ec2-54-227-249-165.compute-1.amazonaws.com",
-    user: "zumbqxstegddgc",
-    port: 5432,
-    password: "8gplBMC7fCJv7LQaruTOo-3OvP"
+  set :database, ENV['DATABASE_URL']
+    # adapter: "postgresql",
+    # database: "d1sn01s5ri49fu",
+    # host: "ec2-54-227-249-165.compute-1.amazonaws.com",
+    # user: "zumbqxstegddgc",
+    # port: 5432,
+    # password: "8gplBMC7fCJv7LQaruTOo-3OvP"
 
-    }
+    
   # Load all models from app/models, using autoload instead of require
   # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
   Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
